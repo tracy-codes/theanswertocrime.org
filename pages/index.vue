7d39ae1512a4a321f376df4bf5e18a70dab23051
Tracy Adams <tracy@tracycodes.com>
2020-01-20 16:17:37 -0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class=" h-screen w-full flex items-center justify-center text-center px-4"
+  >
+    <h1 class="font-bold text-3xl">The Answer to Crime - Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
