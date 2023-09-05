1eb0105520f402641e4938cf7cdbc8b03e586052
Tracy Adams <31976538+tracy-codes@users.noreply.github.com>
2023-09-05 12:18:38 -0700

    Remove gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,41 @@
 module.exports = {
-  siteMetadata: {
-    title: `The Answer To Crime`,
-    description: `Petition for clemency & commutation service.`,
-    author: ``,
-  },
-  plugins: [
-    "gatsby-plugin-eslint",
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `the-answer-to-crime`,
-        short_name: ``,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#4dc0b5`,
-        display: `minimal-ui`,
-        icon: `src/images/light-icon.png`,
-      },
+    siteMetadata: {
+        title: `The Answer To Crime`,
+        description: `Petition for clemency & commutation service.`,
+        author: ``,
     },
-    {
-      resolve: "gatsby-plugin-postcss",
-      options: {
-        postCssPlugins: [
-          require(`tailwindcss`)(`./tailwind.config.js`),
-          require(`autoprefixer`),
-          require(`cssnano`),
-        ],
-      },
-    },
-
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "UA-151745761-3", // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          // optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
+    plugins: [
+        "gatsby-plugin-eslint",
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `the-answer-to-crime`,
+                short_name: ``,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#4dc0b5`,
+                display: `minimal-ui`,
+                icon: `src/images/light-icon.png`,
+            },
         },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        {
+            resolve: "gatsby-plugin-postcss",
+            options: {
+                postCssPlugins: [
+                    require(`tailwindcss`)(`./tailwind.config.js`),
+                    require(`autoprefixer`),
+                    require(`cssnano`),
+                ],
+            },
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        tailwind: true,
-        purgeOnly: [`src/css/style.css`],
-      },
-    },
-    `gatsby-plugin-offline`,
-  ],
+        {
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+                tailwind: true,
+                purgeOnly: [`src/css/style.css`],
+            },
+        },
+        `gatsby-plugin-offline`,
+    ],
 };
